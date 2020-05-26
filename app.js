15aa68359a673c585f84a9036f1d79d3f06a0d3b
Vinaya Kumar <vinay9415756155@gmail.com>
2020-05-26 18:07:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+
+
+/*const request=require("request");
+const https=require("https");*/
+const app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server started at 3000");
+});
